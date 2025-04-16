b3274fc58a223ee8bc2c992f0df26048934b8869
adnene <bendiouisadnane3@gmail.com>
2025-04-16 21:50:40 +0100

    Refactor app.py to include dynamic port configuration for deployment

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 # Initialisation de la base de données
 def init_db():
